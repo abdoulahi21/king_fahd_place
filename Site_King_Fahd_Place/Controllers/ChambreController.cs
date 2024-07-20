84f4d49d9a5638a115b634733d0c949008a52658
abdoulahi21 <abdoualhi.diallo@unchk.edu.sn>
2024-07-20 11:04:35 +0000

    first commit

---
+++ Site_King_Fahd_Place/Controllers/ChambreController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Site_King_Fahd_Place.Models;
+using System.Diagnostics;
+
+namespace Site_King_Fahd_Place.Controllers
+{
+    public class ChambreController : Controller
+    {
+        private readonly ILogger<ChambreController> _logger;
+
+        public ChambreController(ILogger<ChambreController> logger)
+        {
+            _logger = logger;
+        }
+
+      
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Site_King_Fahd_Place/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Site_King_Fahd_Place.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
